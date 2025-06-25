8eff175afe5123d391f0e7537767957db4cd8331
Nghiêm Huy Hưng 11222608 <66253501+Nghiem-HuyHung-11222608@users.noreply.github.com>
2025-06-25 21:50:29 +0700

    Delete studentmanagment/studentmanagment/src/main/java/com/example/studentmanagment directory

--- studentmanagment/studentmanagment/src/main/java/com/example/studentmanagment/repository/LessonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentmanagment.repository;
-
-import com.example.studentmanagment.model.Lesson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonRepository extends JpaRepository<Lesson, Long> {
-}
--- studentmanagment/studentmanagment/src/main/java/com/example/studentmanagment/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.studentmanagment.repository;
-
-import com.example.studentmanagment.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
