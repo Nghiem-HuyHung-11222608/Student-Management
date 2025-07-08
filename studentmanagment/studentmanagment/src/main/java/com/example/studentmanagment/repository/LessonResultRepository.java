11ed522de8d76a6e1e6f9229378b4d513c509761
Nghiêm Huy Hưng 11222608 <66253501+Nghiem-HuyHung-11222608@users.noreply.github.com>
2025-07-08 16:41:01 +0700

    Delete studentmanagment/studentmanagment/src/main/java/com/example/studentmanagment directory

--- studentmanagment/studentmanagment/src/main/java/com/example/studentmanagment/repository/LessonResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentmanagment.repository;
-
-import com.example.studentmanagment.model.LessonResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonResultRepository extends JpaRepository<LessonResult, Long> {
-}
--- studentmanagment/studentmanagment/src/main/java/com/example/studentmanagment/service/LessonResultService.java
+++
@@ -1,8 +0,0 @@
-package com.example.studentmanagment.service;
-
-import com.example.studentmanagment.model.LessonResult;
-
-public interface LessonResultService {
-    LessonResult submitLessonResult(Long lessonId, LessonResult result, Long studentId);
-    LessonResult reviewLessonResult(Long resultId, LessonResult.Status status, String comment, Long teacherId);
-}
